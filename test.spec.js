d3aa935a96a5bacc811c779e780e64b9986a86fa
Code Remote <jordan.8474@gmail.com>
2020-11-16 10:12:41 +0000

    dfsfsad

--- test.spec.js
+++ test.spec.js
@@ -4,6 +4,15 @@ describe("Test", function(){
     }
 });
 
+describe("Naming Length", function(){
+    let str = "";
+    for(var i = 0; i < 99; i++){
+        str += "A";
+    }
+
+    it("test " + str, () => expect(true).toBe(true));
+});
+
 describe("Test 2", function(){
     it("fails", () => expect(true).toBe(false));
 });
